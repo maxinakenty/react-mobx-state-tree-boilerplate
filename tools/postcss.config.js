07ce21040f9c242aa362fed0ee93716f4fc3305e
maxinakenty <mgreen097@gmail.com>
2018-08-20 14:41:46 +0300

    add plugins in postcss config

--- tools/postcss.config.js
+++ tools/postcss.config.js
@@ -3,7 +3,21 @@ const short = require('postcss-short');
 const flexbugs = require('postcss-flexbugs-fixes');
 const customSelectors = require('postcss-custom-selectors');
 const selectorNot = require('postcss-selector-not');
+const vars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const mixins = require('postcss-mixins');
+const cssImport = require('postcss-import');
 
 module.exports = () => ({
-  plugins: [autoprefixer, short, flexbugs, customSelectors, selectorNot],
+  plugins: [
+    autoprefixer,
+    cssImport,
+    short,
+    flexbugs,
+    customSelectors,
+    selectorNot,
+    mixins,
+    nested,
+    vars,
+  ],
 });
